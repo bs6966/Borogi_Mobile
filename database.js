62991191e8bd7d0be9d7c1f73148d77434ac274e
bs6966 <bs6966@srmist.edu.in>
2023-06-23 15:45:11 +0530

    Product Details + Payment integration

---
+++ database.js
@@ -0,0 +1,42 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDYw8agsTmj5x1bz72BlJ-f9YYB1dmmrdg",
+    authDomain: "borogi.firebaseapp.com",
+    databaseURL: "https://borogi-default-rtdb.firebaseio.com",
+    projectId: "borogi",
+    storageBucket: "borogi.appspot.com",
+    messagingSenderId: "49527605540",
+    appId: "1:49527605540:web:6daf22af889ba3a0528bb1"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+var paymentFormDB=firebase.database().ref('borogi')
+
+document.getElementById('paymentForm').addEventListener('submit',submitForm)
+
+function submitForm(event) {
+    event.preventDefault()
+
+    var name = getElementVal('name')
+    var email = getElementVal('email')
+    var contact = getElementVal('contact')
+    var address = getElementVal('address')
+    
+    saveMessages(name,email,contact,address)
+
+}
+
+const saveMessages = (name,email,contact,address) => {
+    var newPyamentForm = paymentFormDB.push()
+
+    newPyamentForm.set({
+        name: name,
+        email: email,
+        contact: contact,
+        address: address
+    })
+}
+
+const getElementVal = (id) => {
+    return document.getElementById(id).value
+}
